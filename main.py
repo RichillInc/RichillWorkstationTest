228c7a5d78f705ba37a5560b1b01f55d5914982b
Richill, Inc <richillinc@gmail.com>
2021-03-11 15:05:05 +0800

    Flash

--- main.py
+++ main.py
@@ -4,3 +4,21 @@
 
 
 
+
+import mysql.connector as mysql 
+
+
+config = {
+    "host": "192.168.1.112",
+    "port": 3306,
+    "user": "lazybong",
+    "password": "among7201"
+}
+
+
+connection = mysql.connect(
+    **config
+)
+
+print(connection)
+
